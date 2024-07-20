ac0edfec974e13441896707ac36db012678b2fc7
junsu0302 <junsu96378224@gmail.com>
2024-07-20 18:35:38 +0900

    #2 - 2층 신경망 동작
    구현한 2층 신경망을 MNIST 데이터로 학습을 수행

---
+++ test.py
@@ -0,0 +1,43 @@
+import numpy as np
+from proto.two_layer_net import TwoLayerNet
+from proto.datasets.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+# 하이퍼파라미터
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+lr = 0.1
+
+iter_per_epoch = max(train_size / batch_size, 1)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+  # 미니배치
+  batch_mask = np.random.choice(train_size, batch_size)
+  x_batch = x_train[batch_mask]
+  t_batch = t_train[batch_mask]
+  
+  # 기울기 계산
+  grad = network.numerical_gradient(x_batch, t_batch)
+
+  # 매개변수 갱신
+  for key in ('W1', 'b1', 'W2', 'b2'):
+    network.params[key] -= lr * grad[key]
+  
+  # 학습 경과 기록
+  loss = network.loss(x_batch, t_batch)
+  train_loss_list.append(loss)
+
+  # 정확도 기록
+  if i % iter_per_epoch == 0:
+    train_acc = network.accuracy(x_train, t_train)
+    test_acc = network.accuracy(x_test, t_test)
+    train_acc_list.append(train_acc)
+    test_acc_list.append(test_acc)
